0c417dbb3381f6f70c2b255fc6d05a8d547ff816
Mariah McLaughlin <hello@mariahmclaughlin.com>
2017-08-25 20:21:12 -0400

    finished pyparagraph, completed and cleaned up file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,5 @@
-# The total number of months included in the dataset --done
-# The total amount of revenue gained over the entire period --done
+# The total number of months included in the dataset
+# The total amount of revenue gained over the entire period
 # The average change in revenue between months over the entire period
 # The greatest increase in revenue (date and amount) over the entire period
 # The greatest decrease in revenue (date and amount) over the entire period
---
+++ PyParagraph/main.py
@@ -0,0 +1,67 @@
+# Import a text file filled with a paragraph of your choosing. --done
+# Approximate word count --done
+# Approximate sentence count
+# Approximate letter count (per word)
+# Average sentence length (in words)
+
+#Dependencies
+# ----------------------
+import os
+
+# Variables 
+# ----------------------
+wordCount = 0
+wordCountList = []
+letterCount = 0
+sentenceLengthList = []
+
+# Main Process 
+# ----------------------
+txtpath = os.path.join("Resources", 'paragraph_1.txt')
+readFile = open(txtpath, "r")
+
+##Original text
+original_file = readFile.read()
+# print(original_file)
+
+#splits words into items in an array and puts into new variable
+wordsFile = original_file.split()
+
+# calculate number of letters in each word and push to array: ex [3,5,6,7,2] ----------
+for word in wordsFile:
+	wordCountList.append(len(word))
+# print(wordCountList)
+
+# calculate appx letter count and adds them, can get average --------
+for letters in wordCountList:
+	letterCount += letters
+# print(letterCount)
+# print(str(letterCount/len(wordCountList)))
+	
+
+# calculate appx sentence count by separating by period and putting in array ---------
+sentenceCount = original_file.split('.')
+new_sentenceCount = sentenceCount[:-1]
+# print(new_sentenceCount)
+
+# calculate sentence length in words -----
+# print(sentenceCount)
+# print(sentenceCount.count(' '))
+#iterates through each sentence and separates by spaces then adds numbers together ------
+for numWords in sentenceCount[:-1]:
+	sentenceLengthList.append(numWords.count(" "))
+	wordCount += numWords.count(" ")
+# print(sentenceLengthList)
+# print(str(wordCount/len(sentenceLengthList)))
+# print(sentenceLengthList)
+
+
+# Printing It All Out... 
+print("Word Analysis")
+print('===================')
+print("Number of words in this passage: " + str(len(wordsFile)))	
+print("Approximate sentence count: " + str(len(new_sentenceCount)))	
+print("Approximate letter count (per word): " + str(letterCount/len(wordCountList)))	
+print("Average sentence length (in words): " + str(wordCount/len(sentenceLengthList)))	
+
+
